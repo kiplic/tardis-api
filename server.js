e448c47192b49a876e08a412d9f1678db1f9f05c
Dan Miller <dev.kiplic@gmail.com>
2015-01-15 14:44:21 -0500

    first pass at routes, what's missing?

--- server.js
+++ server.js
@@ -16,6 +16,71 @@ router.get('/', function (req, res) {
 	});
 });
 
+router.get('/courses', function (req, res) {
+	res.json({
+		message: 'get all courses'
+	});
+});
+
+router.get('/courses/:id', function (req, res) {
+	res.json({
+		message: 'get course by id'
+	});
+});
+
+router.route('/courses/:id/queues')
+	.get(function (req, res) {
+			res.json({
+			message: 'get all queues by course id'
+		});
+	})
+	.post(function (req, res) {
+		res.json({
+			message: 'create a new queue for a course',
+			examples: {
+				name: 'queue_name'
+			},
+			returns: 'id of created queue'
+		});
+	});
+
+router.route('/queues')
+	.get(function (req, res) {
+			res.json({
+			message: 'get all queues'
+		});
+	});
+
+router.route('/queues/:id')
+	.get(function (req, res) {
+		res.json({
+			message: 'get queue by id'
+		});
+	})
+	.patch(function (req, res) {
+		res.json({
+			message: 'update queue by id',
+			examples: [
+				{
+					op: 'push',
+					id: 'dgm3df'
+				},
+				{
+					op: 'pop'
+				},
+				{
+					op: 'remove',
+					id: 'dgm3df'
+				}
+			]
+		});
+	})
+	.delete(function (req, res) {
+		res.json({
+			'delete queue by id'
+		});
+	});
+
 app.use('/v1', router);
 
 app.listen(port);
