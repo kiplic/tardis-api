dc69855333c075a5b37e553ea65dee7fd23f8b84
Hunter Cassidy <hwcassidy@gmail.com>
2015-01-19 23:28:54 -0500

    Add in course actions

--- router/routes/courses.js
+++ router/routes/courses.js
@@ -1,38 +1,71 @@
+var Course = require('../../models/course');
+
 module.exports = function (router) {
 	router.route('/courses')
 		.get(function (req, res) {
-			res.json({
-				message: 'get all courses'
-			});
+            Course.find(function (err, courses) {
+                if (err) res.send(err);
+
+                res.json(courses);
+            });
 		})
 
 		.post(function (req, res) {
-			res.json({
-				message: 'create new course with data: ' + JSON.stringify(req.body)
-			});
+            var course = new Course();
+
+            course.dept = req.body.dept;
+            course.num = req.body.num;
+            course.title = req.body.title;
+            
+            course.save(function (err) {
+                res.json({
+                    message: 'new course created with data: ' + JSON.stringify(req.body)
+                });
+            });
 		});
 
-	router.route('/courses/:id')
+	router.route('/courses/:course_id')
 		.get(function (req, res) {
-			res.json({
-				message: 'get course with id: ' + req.params.id
-			});
+            Course.findById(req.params.course_id, function (err, course) {
+                if (err) res.send(err);
+
+                res.json(course);
+            });
 		})
 
 		.patch(function (req, res) {
-			res.json({
-				message: 'update course with id: ' + req.params.id
-			});
+            Course.findById(req.params.course_id, function (err, course) {
+                if (err) res.send(err);
+
+                // find fields in body then update them
+                for (field in req.body) {
+                    course[field] = req.body[field];
+                }
+
+                course.save(function (err) {
+                    if (err) res.send(err);
+
+                    res.json({
+                        message: 'updated course with id: ' + req.params.course_id + ' with: ' + JSON.stringify(req.body)
+                    });
+                });
+            });
 		})
 
 		.delete(function (req, res) {
-			res.json({
-				message: 'delete course with id: ' + req.params.id
-			});
+            Course.remove({
+                _id: req.params.course_id
+            }, function (err, course) {
+                if (err) res.send(err);
+
+                res.json({
+                    message: 'delete course with id: ' + req.params.id
+                });
+            });
 		});
 
 	// course queues
-	router.route('/courses/:id/queues')
+	router.route('/courses/:course_id/queues')
 		.get(function (req, res) {
 			res.json({
 				message: 'get all queues for course id: ' + req.params.id
@@ -41,7 +74,7 @@ module.exports = function (router) {
 
 		.post(function (req, res) {
 			res.json({
-				message: 'create queue for course id: ' + req.params.id + ' with body: ' + req.body
+				message: 'create queue for course id: ' + req.params.id + ' with body: ' + JSON.stringify(req.body)
 			});
 		});
 };
